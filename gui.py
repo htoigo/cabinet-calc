43db597812f4096fd5f730f64fea545fefc362c5
Harry H. Toigo <hhtoigo@gmail.com>
2018-06-16 00:54:58 -0700

    Rewrote tk code to use grid geometry manager.

--- gui.py
+++ gui.py
@@ -18,78 +18,87 @@ Where will the output go?
 __version__ = '0.1'
 __author__ = 'Harry H. Toigo II'
 
-import tkinter as tk
-import tkinter.ttk as ttk
+from tkinter import *
+from tkinter import ttk
 
 
-class Application(tk.Frame):
-    """A class representing the application (and top-level window)."""
-    def __init__(self, master=None, title=None):
-        if master is None:
-            # Create a new top-level window to be our master
-            master = tk.Tk()
-            if title is not None:
-                master.title(title)
-        super().__init__(master)
-        self.pack()
+class Application(ttk.Frame):
+    """The application, which is a frame within the root window."""
+    def __init__(self, root=None, title='Cabinet Calc'):
+        if root is None:
+            # Create a new root window to be our master
+            self.root = Tk()
+        else:
+            self.root = root
+        super().__init__(self.root, padding=(3, 3))
+        self.root.title(title)
+        self.grid()
         self.create_widgets()
 
     def create_widgets(self):
         self.title_lbl = ttk.Label(self, text='Euro-Style Cabinet Calculator')
-        self.title_lbl.pack()
-        self.job_name_lbl = ttk.Label(self, text='Job Name:')
-        self.job_name_lbl.pack()
-        self.job_name_entry = ttk.Entry(self)
-        self.job_name_entry.pack()
+        self.jobname_lbl = ttk.Label(self, text='Job Name:')
+        self.jobname_ent = ttk.Entry(self)
         # Material
-        self.material_mb = ttk.Menubutton(self, text='Plywood')
-        self.material_mb.pack()
-        self.material_mb.menu = tk.Menu(self.material_mb, tearoff=0)
-        self.material_mb['menu'] = self.material_mb.menu
-        self.material_mb.menu.add_command(label='Melamine')
-        self.material_mb.menu.add_command(label='Granite')
-        self.matl_thickness_lbl = ttk.Label(self, text='Thickness:')
-        self.matl_thickness_lbl.pack()
-        self.matl_thickness_text = tk.StringVar()
-        self.matl_thickness_text.set('0.75')
-        self.matl_thickness_entry = ttk.Entry(self, textvariable=self.matl_thickness_text)
-        self.matl_thickness_entry.pack()
+        self.material = StringVar()
+        self.material.set('Plywood')
+        self.material_lbl = ttk.Label(self, text='Material:')
+        self.material_cbx = ttk.Combobox(self, textvariable=self.material)
+        # self.material_cbx.bind('<<ComboboxSelected>>', lambda x: pass)
+        self.material_cbx['values'] = ('Plywood', 'Melamine', 'Graphite')
+
+        self.thickness_lbl = ttk.Label(self, text='Thickness:')
+        self.thickness = StringVar()
+        self.thickness.set('0.75')
+        self.thickness_ent = ttk.Entry(self, textvariable=self.thickness)
         # Doors
-        # The control variable
-        self.doors_per_cab = tk.IntVar()
+        self.doors_per_cab = IntVar()
         self.doors_per_cab.set(2)
         self.doors_per_cab_lbl = ttk.Label(self, text='Doors per Cabinet:')
-        self.doors_per_cab_lbl.pack()
-        self.doors_per_cab_radio1 = ttk.Radiobutton(self, value=1, text='1',
-                                                    variable=self.doors_per_cab)
-        self.doors_per_cab_radio1.pack()
-        self.doors_per_cab_radio2 = ttk.Radiobutton(self, value=2, text='2',
-                                                    variable=self.doors_per_cab)
-        self.doors_per_cab_radio2.pack()
-        # Total Width
-        self.total_width_lbl = ttk.Label(self, text='W:')
-        self.total_width_lbl.pack()
-        self.total_width_entry = ttk.Entry(self)
-        self.total_width_entry.pack()
-        # Height
-        self.height_lbl = ttk.Label(self, text='H:')
-        self.height_lbl.pack()
-        self.height_entry = ttk.Entry(self)
-        self.height_entry.pack()
-        # Depth
-        self.depth_lbl = ttk.Label(self, text='D:')
-        self.depth_lbl.pack()
-        self.depth_entry = ttk.Entry(self)
-        self.depth_entry.pack()
+        self.doors_per_cab_rad1 = ttk.Radiobutton(self, value=1, text='1',
+                                                  variable=self.doors_per_cab)
+        self.doors_per_cab_rad2 = ttk.Radiobutton(self, value=2, text='2',
+                                                  variable=self.doors_per_cab)
+        self.total_width_lbl = ttk.Label(self, text='Width:')
+        self.total_width_ent = ttk.Entry(self)
+        self.height_lbl = ttk.Label(self, text='Height:')
+        self.height_ent = ttk.Entry(self)
+        self.depth_lbl = ttk.Label(self, text='Depth:')
+        self.depth_ent = ttk.Entry(self)
+
+        self.grid(column=0, row=0, sticky=(N, S, E, W))
+        self.title_lbl.grid(column=0, columnspan=6, row=0)
+        self.jobname_lbl.grid(column=0, columnspan=2, row=1)
+        self.jobname_ent.grid(column=2, columnspan=2, row=1)
+        self.material_lbl.grid(column=0, columnspan=2, row=2)
+        self.material_cbx.grid(column=2, columnspan=2, row=2)
+        self.thickness_lbl.grid(column=4, row=2)
+        self.thickness_ent.grid(column=5, row=2)
+        self.doors_per_cab_lbl.grid(column=0, columnspan=3, row=3)
+        self.doors_per_cab_rad1.grid(column=3, row=3)
+        self.doors_per_cab_rad2.grid(column=4, row=3)
+        self.total_width_lbl.grid(column=0, row=4)
+        self.total_width_ent.grid(column=1, row=4)
+        self.height_lbl.grid(column=2, row=4)
+        self.height_ent.grid(column=3, row=4)
+        self.depth_lbl.grid(column=4, row=4)
+        self.depth_ent.grid(column=5, row=4)
+
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=1)
+        self.columnconfigure(3, weight=1)
+        self.columnconfigure(4, weight=1)
+        self.columnconfigure(5, weight=1)
+        self.rowconfigure(1, weight=1)
 
         # Ttk code:
         # style = ttk.Style()
         # style.configure('BW.TLabel', foreground='black', background='white')
         # self.l3 = ttk.Label(text='Label 3', style='BW.TLabel')
         # self.l3.pack()
-
-#    def print_contents(self, event):
-#        print('Hi. Contents of entry are now ---->', self.contents.get())
 
 
 # root = tk.Tk()
