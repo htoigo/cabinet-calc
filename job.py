7c10bada7fb90701f83c437d08fba8eb26f20047
Harry H. Toigo <hhtoigo@gmail.com>
2018-08-28 20:14:44 -0700

    Various changes to the Job class...
    Removed Job.customer attribute and added an optional Job.description attribute.
    Changed shopfrac_str() calls to dimstr() and dimstr_col() calls. Changed
    Job.description to Job.specification. In the functions defined within the
    Job.specification() function, rennamed the variable used to build the results
    from whatever it was to `result' in each case. Some other miscellaneous changes.

--- job.py
+++ job.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# job.py    -*- coding: utf-8 -*-
 
 """
 job.py
@@ -9,93 +9,111 @@ This module implements the job facilities of cabcalc.
 :copyright: (c) 2018 by Lee Bernard, Harry H. Toigo II.
 :license: MIT, see LICENSE file for more details.
 
-
 """
 
 #__all__ = [max_cabinet_width, door_hinge_gap, cabinet_run, num_cabinets, Run, Job]
 __version__ = '0.1'
 __author__ = 'Harry H. Toigo II'
 
+
 import cabinet
+from dimension_strs import dimstr, dimstr_col
 
 
 class Job:
-    """A customer job."""
+    """A job with name, an optional description, and a run of cabinets."""
 
-    def __init__(self, name, customer, cab_run):
-        # Do we need a unique job.ID? Not now. Job.name must be unique.
+    def __init__(self, name, cab_run, desc=''):
+        # Job.name is required and must be unique, as it is the job ID.
         self.name = name
-        # Do not need customer field:
-        # self.customer = customer
-        # Each job only has ONE run of cabinets.
+        # A description is optional, and by default is the empty string.
+        self.description = desc
+        # For now, each job only has ONE run of cabinets.
         self.cabs = cab_run
 
-    def description(self):
-        """Create a job description."""
+    def specification(self):
+        """Return a full specification of the job as a list of strings."""
 
         def header():
-            hdr = []
-            hdr.append('Job Name: ' + self.name)
-            return hdr
+            result = []
+            result.append('Job Name: ' + self.name)
+            if self.description is not '':
+                result.append('Description: ' + self.description)
+            return result
 
         def overview():
-            overvw = []
-            overvw.append('Overview:\n')
-            overvw.append('Total Wall Space: ' + str(self.cabs.fullwidth) + '"')
-            overvw.append(str(self.cabs.num_cabinets) + ' cabinets measuring '
-                          + str(self.cabs.cabinet_width) + '" totalling '
-                          + str(self.cabs.cabinet_width * self.cabs.num_cabinets)
-                          + '"')
+            result = []
+            result.append('Overview:\n')
+            result.append('Total Wall Space: ' + str(self.cabs.fullwidth) + '"')
+            result.append(str(self.cabs.num_cabinets) + ' cabinets measuring '
+                          + dimstr(self.cabs.cabinet_width) + '", '
+                          + 'totalling '
+                          + dimstr(self.cabs.cabinet_width
+                                   * self.cabs.num_cabinets) + '"')
             if self.cabs.num_fillers == 0:
-                overvw.append('With finished end panels on left and right')
-                overvw.append('No filler panels required')
+                result.append('with finished end panels on left and right.')
+                result.append('No filler panels required.')
             elif self.cabs.num_fillers == 1:
-                overvw.append('with a ' + str(self.cabs.filler_width)
-                              + '" filler')
+                result.append('with a ' + dimstr(self.cabs.filler_width)
+                              + '" filler.')
             elif self.cabs.num_fillers == 2:
-                overvw.append('with two (2) ' + str(self.cabs.filler_width)
-                              + '" fillers')
+                result.append('with two (2) '
+                              + dimstr(self.cabs.filler_width)
+                              + '" fillers.')
             else:
-                # Raise an exceptiong for having fillers in the middle of a run:
-                print('Your layout would have filler in the middle of a run.\n')
-            overvw.append('\nMaterial thickness: '
-                          + str(self.cabs.matl_thickness) + '"')
-            return overvw
+                # The number of fillers should never be greater than 2
+                raise ValueError('number of fillers not 0, 1, or 2')
+
+            result.append('\nMaterial thickness: '
+                          + dimstr(self.cabs.matl_thickness) + '"')
+            return result
 
         def partslist():
-            """Create a parts list, including doors."""
-            parts = []
-            parts.append('Parts List:\n')
-            parts.append(str(self.cabs.num_backpanels) + " @ "
-                         + str(self.cabs.back_width) + '" x '
-                         + str(self.cabs.back_height) + '" x '
-                         + str(self.cabs.back_thickness) + '" -- BACK PNLS')
-            parts.append(str(self.cabs.num_bottompanels) + " @ "
-                         + str(self.cabs.bottom_width) + '" x '
-                         + str(self.cabs.bottom_depth) + '" x '
-                         + str(self.cabs.bottom_thickness) + '" -- BOTTOM PNLS')
-            parts.append(str(self.cabs.num_sidepanels) + " @ "
-                         + str(self.cabs.side_depth) + '" x '
-                         + str(self.cabs.side_height) + '" x '
-                         + str(self.cabs.side_thickness) + '" -- SIDE PNLS')
-            parts.append(str(self.cabs.num_topnailers) + " @ "
-                         + str(self.cabs.topnailer_width) + '" x '
-                         + str(self.cabs.topnailer_depth) + '" x '
-                         + str(self.cabs.topnailer_thickness)
-                         + '" -- TOP NAILERS')
+            result = []
+            result.append('Parts List:\n')
+            result.append(
+                'Back Panels:     {:2d}  @  {:10s}  x  {:10s}  x  {}'.format(
+                    self.cabs.num_backpanels,
+                    dimstr_col(self.cabs.back_width) + '"',
+                    dimstr_col(self.cabs.back_height) + '"',
+                    dimstr(self.cabs.back_thickness) + '"') )
+            result.append(
+                'Bottom Panels:   {:2d}  @  {:10s}  x  {:10s}  x  {}'.format(
+                    self.cabs.num_bottompanels,
+                    dimstr_col(self.cabs.bottom_width) + '"',
+                    dimstr_col(self.cabs.bottom_depth) + '"',
+                    dimstr(self.cabs.bottom_thickness) + '"') )
+            result.append(
+                'Side Panels:     {:2d}  @  {:10s}  x  {:10s}  x  {}'.format(
+                    self.cabs.num_sidepanels,
+                    dimstr_col(self.cabs.side_depth) + '"',
+                    dimstr_col(self.cabs.side_height) + '"',
+                    dimstr(self.cabs.side_thickness) + '"') )
+            result.append(
+                'Top Nailers:     {:2d}  @  {:10s}  x  {:10s}  x  {}'.format(
+                    self.cabs.num_topnailers,
+                    dimstr_col(self.cabs.topnailer_width) + '"',
+                    dimstr_col(self.cabs.topnailer_depth) + '"',
+                    dimstr(self.cabs.topnailer_thickness) + '"') )
             if self.cabs.num_fillers > 0:
-                parts.append(str(self.cabs.num_fillers) + ' @ '
-                             + str(self.cabs.filler_width) + '" x '
-                             + str(self.cabs.filler_height) + '" x '
-                             + str(self.cabs.filler_thickness) + '" -- FILLERS')
-            parts.append('Door Details:')
-            parts.append(str(self.cabs.num_doors) + " @ "
-                         + str(self.cabs.door_width) + '" x '
-                         + str(self.cabs.door_height) + '" x '
-                         + str(self.cabs.door_thickness) + '" -- DOORS')
-            return parts
+                result.append(
+                    'Fillers:         {:2d}  @  {:10s}  x  {:10s}  x  {}'.format(
+                        self.cabs.num_fillers,
+                        dimstr_col(self.cabs.filler_width) + '"',
+                        dimstr_col(self.cabs.filler_height) + '"',
+                        dimstr(self.cabs.filler_thickness) + '"') )
+            result.append(
+                'Doors:           {:2d}  @  {:10s}  x  {:10s}  x  {}'.format(
+                    self.cabs.num_doors,
+                    dimstr_col(self.cabs.door_width) + '"',
+                    dimstr_col(self.cabs.door_height) + '"',
+                    dimstr(self.cabs.door_thickness) + '"') )
+            return result
 
         sep = '-' * 60
-        desc = ( [sep] + header() + [sep]
+        spec = ( [sep] + header() + [sep]
                  + overview() + [sep] + partslist() + [sep] )
-        return desc
+        return spec
+
+
+# job.py ends here
