f27a41aa1631acce97f501152c12200a1666d1e4
Harry H. Toigo <hhtoigo@gmail.com>
2018-09-06 21:27:12 -0700

    Inserted FSF-recommended boilerplate notice at the top of all source files.

--- cabcalc/job.py
+++ cabcalc/job.py
@@ -1,5 +1,31 @@
 # job.py        -*- coding: utf-8 -*-
 
+# Module with Job class and job-related functions for Cabinet Calc.
+
+# Copyright © 2018  Harry H. Toigo II
+
+# This file is part of Cabinet Calc, the ....
+
+# Cabinet Calc is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Cabinet Calc is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Cabinet Calc.  If not, see <https://www.gnu.org/licenses/>.
+
+# To contact us:
+#
+# Email:       hhtpub@gmail.com
+# Snail mail:  433 Buena Vista Ave. #310
+#              Alameda CA  94501
+
+
 """
 job.py
 ~~~~~~
