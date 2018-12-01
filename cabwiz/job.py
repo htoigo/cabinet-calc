e03e9bab3fadca7ef413a35a56927a523747453d
Harry H. Toigo <hhtoigo@gmail.com>
2018-12-01 12:05:38 -0800

    Fold thickness into primary and door material lines in job overview.

--- cabwiz/job.py
+++ cabwiz/job.py
@@ -100,12 +100,14 @@ class Job:
     def materialinfo(self):
         """A list of strings."""
         result = []
-        result.append('Primary Material:  ' + self.cabs.prim_material)
-        result.append('Primary thickness:  '
-                      + dimstr(self.cabs.prim_thickness) + '"')
-        result.append('Door Material:  ' + self.cabs.door_material)
-        result.append('Door thickness:  '
-                      + dimstr(self.cabs.door_thickness) + '"')
+        result.append('Primary Material:  ' + dimstr(self.cabs.prim_thickness)
+                      + '" ' + self.cabs.prim_material)
+        # result.append('Primary thickness:  '
+        #               + dimstr(self.cabs.prim_thickness) + '"')
+        result.append('Door Material:  ' + dimstr(self.cabs.door_thickness)
+                      + '" ' + self.cabs.door_material)
+        # result.append('Door thickness:  '
+        #               + dimstr(self.cabs.door_thickness) + '"')
         return result
 
     @property
