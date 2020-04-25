8dfdaacbbb670db53da7fcc439dd6d456e5d3c75
Harry H. Toigo <hhtoigo@gmail.com>
2020-04-24 23:16:10 -0700

    Added tests/context.py to provide import context for tests.
    This is per the recommendation of Kenneth Reitz in the Hitchhiker's Guide to
    Python. In tests/context.py, we use an explicit system path modification so that
    test modules in the tests/ directory can import app modules from the app/module
    directory cabwiz/.

---
+++ tests/context.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+
+sys.path.insert(0,
+    os.path.abspath(
+        os.path.join.(os.path.dirname(__file__), '..')
+    )
+)
+
+
+import cabinet
+import cutlist
+import dimension_strs
+import gui
+import job
+import text
--- tests/test_dimension_strs.py
+++ tests/test_dimension_strs.py
@@ -1,56 +1,59 @@
 # test_dimension_strs.py    -*- coding: utf-8 -*-
 
 
-import unittest
-from dimension_strs import dimstr, dimstr_col, sdalign
+# import unittest
+import pytest
+from .context import dimension_strs as DS
+#      import dimstr, dimstr_col, sdalign
+# from dimension_strs import dimstr, dimstr_col, sdalign
 
 
 class DimstrTest(unittest.TestCase):
 
     def test_zero(self):
-        self.assertEqual(dimstr(0), '0')
+        self.assertEqual(DS.dimstr(0), '0')
 
     def test_one_fourth(self):
-        self.assertEqual(dimstr(0.25), '1/4')
+        self.assertEqual(DS.dimstr(0.25), '1/4')
 
     def test_one_eighth(self):
-        self.assertEqual(dimstr(0.125), '1/8')
+        self.assertEqual(DS.dimstr(0.125), '1/8')
 
     def test_eleven_and_two_sevenths(self):
-        self.assertEqual(dimstr(11 + 2/7), '11 5/16-')
+        self.assertEqual(DS.dimstr(11 + 2/7), '11 5/16-')
 
 
 class Dimstr_colTest(unittest.TestCase):
 
     def test_zero(self):
-        self.assertEqual(dimstr_col(0), ' 0')
+        self.assertEqual(DS.dimstr_col(0), ' 0')
 
     def test_one(self):
-        self.assertEqual(dimstr_col(1), ' 1')
+        self.assertEqual(DS.dimstr_col(1), ' 1')
 
     def test_nine(self):
-        self.assertEqual(dimstr_col(9), ' 9')
+        self.assertEqual(DS.dimstr_col(9), ' 9')
 
     def test_one_fourth(self):
-        self.assertEqual(dimstr_col(0.25), '1/4')
+        self.assertEqual(DS.dimstr_col(0.25), '1/4')
 
     def test_point_two_seven(self):
-        self.assertEqual(dimstr_col(0.27), '1/4+')
+        self.assertEqual(DS.dimstr_col(0.27), '1/4+')
 
 
 class SDAlignTest(unittest.TestCase):
 
     def test_four(self):
-        self.assertEqual(sdalign('4'), ' 4')
+        self.assertEqual(DS.sdalign('4'), ' 4')
 
     def test_eight_and_three_fourths(self):
-        self.assertEqual(sdalign('8 3/4'), ' 8 3/4')
+        self.assertEqual(DS.sdalign('8 3/4'), ' 8 3/4')
 
     def test_double_digit_mixed(self):
-        self.assertEqual(sdalign('22 3/8'), '22 3/8')
+        self.assertEqual(DS.sdalign('22 3/8'), '22 3/8')
 
     def test_double_digit_whole(self):
-        self.assertEqual(sdalign('17'), '17')
+        self.assertEqual(DS.sdalign('17'), '17')
 
 
 if __name__ == '__main__':
