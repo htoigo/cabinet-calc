b0859dc8d11e45504c5b3cb8f97181105c2c9309
Harry H. Toigo <hhtoigo@gmail.com>
2020-05-01 22:55:43 -0700

    Added new test file tests/test_cabinet.py to test cabinet module.

---
+++ tests/test_cabinet.py
@@ -0,0 +1,17 @@
+# test_cabinet.py    -*- coding: utf-8 -*-
+
+import sys
+import os
+
+# Add the root directory of the project to sys.path so that we can find modules
+# in other subtrees (outside of tests dir) for importing.
+sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir))
+
+import app.cabinet as C
+
+def test_num_cabinets():
+    cr = C.Run(157.25, 28.5, 24.0)
+    assert cr.num_cabinets == 5
+
+
+# test_cabinet.py ends here
