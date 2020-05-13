afdf908406730e5f7e3351d99007e0e9d0961933
Harry H. Toigo <hhtoigo@gmail.com>
2020-05-12 22:56:51 -0700

    Added file tests/test_text.py for testing the text.py module.

---
+++ tests/test_text.py
@@ -0,0 +1,24 @@
+# test_text.py    -*- coding: utf-8 -*-
+
+
+from reportlab.rl_config import canvas_basefontname as _baseFontName
+from reportlab.lib.enums import TA_LEFT, TA_CENTER, TA_RIGHT, TA_JUSTIFY
+
+import app.text as T
+
+
+def test_normal_style():
+    assert T.normal_style.name == 'Normal'
+    assert T.normal_style.fontName == _baseFontName
+    assert T.normal_style.fontSize == 10
+    assert T.normal_style.leading == 12
+    assert T.normal_style.alignment == TA_LEFT
+
+
+def test_rt_style():
+    assert T.rt_style.name == 'RightText'
+    assert T.rt_style.parent == T.normal_style
+    assert T.rt_style.alignment == TA_RIGHT
+
+
+# test_text.py  ends here
